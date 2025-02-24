d00df21af1d6a8e117f0609d702a41c3847589fa
MailonCadene <vendas7@terris.com.br>
2025-02-24 17:34:06 -0300

    atualização env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://lmwojzkjtrsypgbwzvgw.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxtd29qemtqdHJzeXBnYnd6dmd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0MDcxNDcsImV4cCI6MjA1NTk4MzE0N30.CpD61ZNwY77A9VaKVd3lSmlJBjwuGnWKA1OJWYMDG9I';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
